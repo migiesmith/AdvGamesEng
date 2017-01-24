a592197e58af840a5ac8737b1ebcadbebe692b9d
migiesmith <migiesmith@gmail.com>
2017-01-24 01:37:32 +0000

    Updated the Menu Scene
    Contains one interactable and random stars

---
+++ [Space]/Assets/Menu/Scripts/MenuShipController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuShipController : MonoBehaviour {
+
+	private Vector3 initialPos;
+
+	private bool returnToStart = false;
+
+	// Use this for initialization
+	void Start () {
+		this.initialPos = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// If this is not being held by the player
+		if(returnToStart){
+			// Move towards the initial position
+			this.transform.position = Vector3.Lerp(this.transform.position, initialPos, 0.1f);
+
+			// If close to initial position, snap to it
+			if(Vector3.Magnitude(this.transform.position - initialPos) <= 0.1f){
+				this.transform.position = initialPos;
+				returnToStart = false;
+			}
+		}
+	}
+
+	public void OnPickUp(){
+		returnToStart = false;
+	}
+
+	public void OnDrop(){
+		returnToStart = true;
+	}
+
+}
---
+++ [Space]/Assets/Menu/Scripts/StarSphereController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarSphereController : MonoBehaviour {
+
+	public float radius = 3.0f;
+
+
+	private ParticleSystem partSys;
+	private bool hasStarted = false;
+
+
+	// Use this for initialization
+	void Start () {			
+			// Get the Particle System attatched to this
+			partSys = this.GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		var emitParams = new ParticleSystem.EmitParams();
+		emitParams.startLifetime = Mathf.Infinity;
+		partSys.Emit(emitParams, partSys.main.maxParticles);
+
+		if(!hasStarted){
+			int numParticles = partSys.main.maxParticles;
+			
+			// Create a list to store the particles
+			ParticleSystem.Particle[] particles = new ParticleSystem.Particle[numParticles];
+			
+			partSys.GetParticles (particles);
+
+			for(int i = 0; i < numParticles; i++) {
+				ParticleSystem.Particle p = particles[i];
+				p.position = Random.insideUnitSphere * radius;
+				p.startColor = new Color(1, 1, 1, 0.3f + p.position.magnitude * 0.7f);
+				float s = Random.Range(0.01f, 0.05f);
+				p.startSize3D = new Vector3(s,s,s);
+				particles[i] = p;
+			}
+			partSys.SetParticles(particles, numParticles);
+
+			hasStarted = true;
+		}else{
+
+		}
+
+
+	}
+
+}
