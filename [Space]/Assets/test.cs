49557cd7e5e83cbaab477a31bde6bb51ebaad4cd
migiesmith <migiesmith@gmail.com>
2017-01-25 12:54:26 +0000

    Teleport Arc Trajectory

--- [Space]/Assets/test.cs
+++ [Space]/Assets/test.cs
@@ -4,34 +4,56 @@ using UnityEngine;
 
 public class test : MonoBehaviour {
 
-
 	public int points = 9;
-	public float dist = 1.0f;
+
+	public float strength = 10.0f;
+
+	public Transform target;
 
 	private LineRenderer lineRend;
+
 
 	// Use this for initialization
 	void Start () {
 		lineRend = this.GetComponent<LineRenderer>();
-		lineRend.numPositions = points;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		Vector3 vel = new Vector3(4.0f*(Mathf.Sin(Time.time) + 1.0f), Physics2D.gravity.magnitude/2.0f,0);
-		float velocity = Mathf.Sqrt((vel.x * vel.x) + (vel.y * vel.y));
-        float angle = Mathf.Rad2Deg*(Mathf.Atan2(vel.y , vel.x));
-        float fTime = 0.2f;
-        
-		lineRend.SetPosition(0, new Vector3(0,0,0));
-        for (int i = 1 ; i < points; i++) {
-            float dx = velocity * fTime * Mathf.Cos(angle * Mathf.Deg2Rad);
-            float dy = velocity * fTime * Mathf.Sin(angle * Mathf.Deg2Rad) - (Physics2D.gravity.magnitude * fTime * fTime / 2.0f);
-            Vector3 pos = new Vector3(dx , dy, 0);
+		// Set the number of points we want on the line
+		lineRend.numPositions = points;
 
+		// Calculate the angle of the arc
+		// Z-Axis
+		float angle = Mathf.PI/2.0f - Vector3.Dot(Vector3.Normalize(new Vector3(0, target.position.y, target.position.z)), new Vector3(0, 0, 1));
+		// X-Axis
+		float angle2 =  Mathf.PI/2.0f - Vector3.Dot(Vector3.Normalize(new Vector3(target.position.x, target.position.y, 0)), new Vector3(1, 0, 0));
+
+		// Set angle to angle2 if we are closer to the X-Axis
+		if(Mathf.Abs(target.transform.position.x) > Mathf.Abs(target.transform.position.z)){
+			angle = angle2;
+		}
+
+		// Calculate the velocity
+		float vV = strength * Mathf.Sin(angle); // Vertical - Y
+		float vH = strength * Mathf.Cos(angle); // Horizontal - X and Z
+
+		// Store the progress through the arc
+		float t = 0.0f;
+
+        for (int i = 0 ; i < points; i++) {
+			// Horizontal Displacement - X and Z
+			float dH = vH * t;
+			// Vertical Displacement - Y
+			float dV = (vV * t) + 0.5f * Physics.gravity.y * t * t;
+			// Create the position for the line
+			Vector3 pos = Vector3.Normalize(new Vector3(target.position.x, 0, target.position.z)) * Mathf.Abs(dH) + this.transform.up * dV;			
+			// Increment arc progress
+			t += (-vV / Physics.gravity.y * 2.0f) / (points - 1);
+			// Set the line position
 			lineRend.SetPosition(i, pos);
-            fTime += 1.0f / points;
-        }
+		}
+		
 
 	}
 }
