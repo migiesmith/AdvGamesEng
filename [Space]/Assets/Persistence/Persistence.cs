bb0dd7a9d32483f79efb0818592591aad3c25727
Mcguire <40121777@live.napier.ac.uk>
2017-02-16 16:15:22 +0000

    Started Persistence

---
+++ [Space]/Assets/Persistence/Persistence.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public class Persistence : MonoBehaviour {
+
+
+    void saveGame()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Open(Application.persistentDataPath + "/playerInfo.dat", FileMode.Open);
+    }
+
+    void loadGame()
+    {
+
+    }
+
+}
+
+[Serializable]
+class PlayerData
+{
+
+}
