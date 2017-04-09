16b1c009fb3b9a9247afe511cce0132c8b229807
Unknown <robert.a.brown117@gmail.com>
2017-04-09 18:55:05 +0100

    Inventory Harness Rework
    Removed fixed joint setup on the inventory harness. Inventory slots correctly follow the player again and should no longer be vulnerable to explosions or other physics issues.

--- [Space]/Assets/_Scripts/Menus & Inventories/Inventories/Consumable Inventory/ConsumableInventory.cs
+++ [Space]/Assets/_Scripts/Menus & Inventories/Inventories/Consumable Inventory/ConsumableInventory.cs
@@ -7,16 +7,24 @@ namespace space
 {
     public class ConsumableInventory : MonoBehaviour
     {
+        private NVRHead head;
         public Dictionary<GameObject, int> inventoryList = new Dictionary<GameObject, int>();
         public GameObject[] inventoryItems;
         public int startCount = 5;
         // Use this for initialization
         void Start()
         {
+            head = transform.root.GetComponentInChildren<NVRHead>();
             foreach(GameObject g in inventoryItems)
             {
                 inventoryList.Add(g, startCount);
             }
+        }
+
+        void Update()
+        {
+            transform.localPosition = new Vector3(head.transform.localPosition.x, transform.localPosition.y, head.transform.localPosition.z);
+            transform.localRotation = new Quaternion(transform.localRotation.x, head.transform.localRotation.y, transform.localRotation.z, transform.localRotation.w);
         }
 
         public Dictionary<GameObject, int> setConsumables()
