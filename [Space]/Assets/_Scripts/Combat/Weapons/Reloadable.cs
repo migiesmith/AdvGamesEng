61aab55ab7d56692bacd20f9f1071dd9aab1f0f8
Unknown <robert.a.brown117@gmail.com>
2017-04-10 13:53:52 +0100

    Improved Reload Mechanics
    Magazines no longer instantly snap into place when reloading. When a compatible magazine is detected at the opening of the magwell, the magazine is automatically aligned with the magwell chamber and magazine movement is constrained to slide along the central axis of the magwell. Sliding the magazine to the top of the magwell reloads as before.

--- [Space]/Assets/_Scripts/Combat/Weapons/Reloadable.cs
+++ [Space]/Assets/_Scripts/Combat/Weapons/Reloadable.cs
@@ -8,6 +8,9 @@ namespace space
     public class Reloadable : MonoBehaviour
     {
         private Transform magwell;
+        private Transform magentry;
+        private Vector2 magrail;
+        public Collider mainCol;
 
         public float ammoCapacity = 24;
         public float ammoCount;
@@ -22,60 +25,98 @@ namespace space
         public AudioSource magIn;
         public AudioSource magOut;
 
+        private bool sliding;
+
         // Use this for initialization
         void Start()
         {
             ammoCount = 0;
             magwell = transform.FindChild(name + "_Magwell");
+            magentry = magwell.transform.FindChild(name + "_Magentry");
+            magrail = new Vector2(magentry.localPosition.y, magentry.localPosition.z);
+            sliding = false;
+        }
+
+        void Update()
+        {
+            if (sliding)
+            {
+                float magScalar = Vector2.Dot(new Vector2(magazine.transform.localPosition.y, magazine.transform.localPosition.z), magrail.normalized);
+                if (magScalar <= 0 && magazine.name != "Empty")
+                {
+                    reload();
+                }
+                else
+                {
+                    Vector2 magProjection = magrail.normalized * magScalar;
+                    if (magProjection.magnitude > magrail.magnitude + 0.1f)
+                    {
+                        Physics.IgnoreCollision(mainCol, magCol, false);
+                        magazine.transform.parent = null;
+                        magazine = null;
+                        sliding = false;
+                    }
+                    else
+                    {
+                        magazine.transform.localPosition = new Vector3(0.0f, magProjection.x, magProjection.y);
+                        magazine.transform.localRotation = new Quaternion(0.0f, 0.0f, 0.0f, 1.0f);
+                    }
+                }
+            }
         }
 
         public void ejectMag()
         {
-            if (magazine != null)
+            if (magazine != null && !sliding)
             {
                 magazine.name = "Empty";
-                magazine.transform.parent = null;
                 magRB.useGravity = true;
                 magRB.isKinematic = false;
-                magCol.enabled = true;
                 magInt.enabled = false;
                 Destroy(magazine.gameObject, 10.0f);
-                magazine = null;
                 magOut.Play();
+                sliding = true;
             }
+        }
+
+        public void reload()
+        {
+            if (magInt != null)
+            {
+                magInt.ForceDetach();
+                magInt.AttachedHand = null;
+            }
+            if (magRB != null)
+            {
+                magRB.useGravity = false;
+                magRB.isKinematic = true;
+            }
+            magazine.transform.localPosition = new Vector3(0, 0, 0); ;
+            magazine.transform.localRotation = new Quaternion(0, 0, 0, 1);
+            magazine.transform.localScale = new Vector3(1, 1, 1);
+
+            ammoCount = ammoCapacity;
+            magIn.Play();
+
+            sliding = false;
         }
 
         private void OnTriggerEnter(Collider magDetect)
         {
-            if (magDetect.gameObject.name == magPrefab.gameObject.name && magazine == null)
+            if (magDetect.gameObject.name.Equals(magPrefab.name) && magazine == null)
             {
                 magazine = magDetect.gameObject;
-
-                magInt = magDetect.gameObject.GetComponent<NVRInteractableItem>();
-                if (magInt != null)
-                {
-                    magInt.ForceDetach();
-                    magInt.AttachedHand = null;
-                }
-
+                magInt = magazine.GetComponent<NVRInteractableItem>();
                 magRB = magazine.GetComponent<Rigidbody>();
-                if (magRB != null)
-                {
-                    magRB.useGravity = false;
-                    magRB.isKinematic = true;
-                }
-
                 magCol = magazine.GetComponent<Collider>();
-                if (magCol != null)
-                    magCol.enabled = false;
 
+                Physics.IgnoreCollision(mainCol, magCol);
                 magazine.transform.parent = magwell;
-                magDetect.gameObject.transform.localPosition = new Vector3(0, 0, 0); ;
-                magDetect.gameObject.transform.localRotation = new Quaternion(0, 0, 0, 1);
-                magDetect.gameObject.transform.localScale = new Vector3(1, 1, 1);
+                magazine.transform.localPosition = magentry.transform.localPosition;
+                magazine.transform.localRotation = new Quaternion(0.0f, 0.0f, 0.0f, 1.0f);
+                magazine.transform.localScale = Vector3.one;
 
-                ammoCount = ammoCapacity;
-                magIn.Play();
+                sliding = true;
             }
         }
     }
