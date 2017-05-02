06b849771fe9605a9090cbeab49f8524a4ef8c72
migiesmith <migiesmith@gmail.com>
2017-05-02 01:39:46 +0100

    VoidShot
    Explodes and sucks in enemies.
    Added TemporaryRigidbodyState to allow for temporary modifications to a rigidbody.

---
+++ [Space]/Assets/TemporaryRigidbodyState.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TemporaryRigidbodyState : MonoBehaviour
+{
+
+    public float duration = 1.0f;
+    private bool wasKinematic = true;
+    public bool isKinematic = true;
+
+    private bool wasUsingGravity = true;
+    public bool useGravity = true;
+
+    public RigidbodyConstraints constraints;
+    private RigidbodyConstraints originalConstraints;
+
+    private Rigidbody rb;
+
+    private bool beenSet = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        set();
+    }
+
+    public void set()
+    {
+        if (!beenSet)
+        {
+            rb = GetComponent<Rigidbody>();
+            Debug.Log(GetComponents<TemporaryRigidbodyState>().Length);
+            if (rb == null || GetComponents<TemporaryRigidbodyState>().Length > 1)
+            {
+                // This script can't be ran, destroy it
+                Destroy(this);
+            }
+            else
+            {
+                // Copy the current state
+                wasKinematic = rb.isKinematic;
+                wasUsingGravity = rb.useGravity;
+                originalConstraints = rb.constraints;
+                // Set the temporary state
+                rb.isKinematic = isKinematic;
+                rb.useGravity = useGravity;
+                rb.constraints = constraints;
+                // Tell the system to destroy this script after a duration
+                Destroy(this, duration);
+                beenSet = true;
+            }
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (beenSet)
+        {
+            // Restore the initial state
+            rb.isKinematic = wasKinematic;
+            rb.useGravity = wasUsingGravity;
+            rb.constraints = originalConstraints;
+        }
+    }
+
+}
---
+++ [Space]/Assets/VoidShot.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoidShot : MonoBehaviour
+{
+
+    public float explodeTime = 1.0f;
+    [Range(0.0f, 1.0f)] public float primeRatio = 0.9f;
+    private float timeToExplode;
+    public float explosionDuration = 1.0f;
+    private float timeTillExplosionOver;
+    public float suctionRange = 1.0f;
+    public float suctionPower = 500.0f;
+
+	public LayerMask layerMask;
+
+    public Scale prime;
+    public GameObject explosion;
+
+    // Use this for initialization
+    void Start()
+    {
+        prime.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+        prime.scaleUpEnd.AddListener(primed);
+        prime.scaleDownEnd.AddListener(explode);
+        explosion.SetActive(false);
+    }
+
+    void explode()
+    {
+        explosion.SetActive(true);
+        Rigidbody[] rigidbodies = FindObjectsOfType<Rigidbody>();
+        foreach (Rigidbody rb in rigidbodies)
+        {
+            Vector3 dir = (rb.transform.position - this.transform.position);
+            RaycastHit hitInfo;
+			if (rb.GetComponent<ShieldController>() == null && Physics.Raycast(this.transform.position, (dir).normalized, out hitInfo, 10, layerMask))
+            {
+                if (hitInfo.collider.GetComponent<Rigidbody>() == rb && rb.tag != "Player")
+                {
+					Enemy enemy = rb.GetComponent<Enemy>();
+					if(enemy != null)
+					{
+						enemy.ToAlert();
+						enemy.lastKnownLocation = enemy.transform.position;
+					}
+					TemporaryRigidbodyState rbState = rb.gameObject.AddComponent<TemporaryRigidbodyState>();
+					rbState.isKinematic = false;
+					rbState.duration = 2.0f;
+					rbState.set();
+
+                    // Apply suction force
+                    rb.AddExplosionForce(dir.magnitude * -suctionPower, this.transform.position, suctionRange, 0.0f, ForceMode.Acceleration);
+                    Debug.DrawLine(this.transform.position, rb.transform.position);
+                }
+            }
+        }
+    }
+
+    public void trigger()
+    {
+        timeToExplode = explodeTime;
+        prime.scaleDuration = explodeTime * primeRatio;
+        prime.setScale(1.0f);
+    }
+
+    public void primed()
+    {
+        prime.scaleDuration = explodeTime * (1.0f - primeRatio);
+        prime.setScale(0.01f);
+        timeTillExplosionOver = explosionDuration;
+        explosion.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            trigger();
+        }
+        if (timeToExplode > 0.0f)
+        {
+            timeToExplode -= Time.deltaTime;
+            if (timeToExplode <= 0.0f)
+            {
+                explode();
+            }
+        }
+        else if (timeTillExplosionOver > 0.0f)
+        {
+            timeTillExplosionOver -= Time.deltaTime;
+            if (timeTillExplosionOver <= 0.0f)
+            {
+                explosion.SetActive(false);
+            }
+        }
+    }
+}
